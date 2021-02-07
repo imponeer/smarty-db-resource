2b436d9e2644e11e2127f769c0c7dd8ab7ad4dc6
Raimondas Rimkevičius <github@mekdrop.name>
2021-02-07 17:48:58 +0200

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# smarty-db-resource
-Smarty plugin that adds DB resource type
+[![License](https://img.shields.io/github/license/imponeer/smarty-db-resource.svg)](LICENSE)
+[![GitHub release](https://img.shields.io/github/release/imponeer/smarty-db-resource.svg)](https://github.com/imponeer/smarty-db-resource/releases) [![Maintainability](https://api.codeclimate.com/v1/badges/c284ca86c6df6e98b9d0/maintainability)](https://codeclimate.com/github/imponeer/smarty-db-resource/maintainability) [![PHP](https://img.shields.io/packagist/php-v/imponeer/smarty-db-resource.svg)](http://php.net) 
+[![Packagist](https://img.shields.io/packagist/dm/imponeer/smarty-db-resource.svg)](https://packagist.org/packages/imponeer/smarty-db-resource)
+
+# Smarty DB resource
+
+[Smarty](https://smarty.net) resource plugin that can read templates from database.
+
+This plugin is inspired but similar from [Xoops](https://xoops.org) - [resource.db](https://github.com/XOOPS/XoopsCore25/blob/v2.5.8/htdocs/class/smarty/xoops_plugins/resource.db.php).
+
+## Installation
+
+To install and use this package, we recommend to use [Composer](https://getcomposer.org):
+
+```bash
+composer require imponeer/smarty-db-resource
+```
+
+Otherwise, you need to include manually files from `src/` directory. 
+
+## Registering in Smarty
+
+If you want to use these extensions from this package in your project you need register them with [`registerResource` function](https://www.smarty.net/docs/en/api.register.resource.tpl) from [Smarty](https://www.smarty.net). For example:
+```php
+$smarty = new \Smarty();
+$plugin = new \Imponeer\Smarty\Extensions\DBResource\DBResource(
+    $cacheItemPool, // psr-6 compatible cache instance
+    $pdo, // PDO compatible database connection
+    'default', // current template set name
+    'tplfile',
+    'tpl_source,
+    'tpl_lastmodified',
+    'tpl_tplset',
+    'tpl_file',
+    function (array $row): string { // function that converts database row info into string of real file
+       return $row['file'];
+    },
+    'default'
+);
+$smarty->registerResource($plugin->getName(), $plugin);
+```
+
+## Using from templates
+
+To use this resource from templates, you need to use `db:` prefix when accessing files. For example :
+```smarty
+  {include file="db:/images/image.tpl"}
+```
+
+## How to contribute?
+
+If you want to add some functionality or fix bugs, you can fork, change and create pull request. If you not sure how this works, try [interactive GitHub tutorial](https://try.github.io).
+
+If you found any bug or have some questions, use [issues tab](https://github.com/imponeer/smarty-db-resource/issues) and write there your questions.
---
+++ src/DBResource.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace Imponeer\Smarty\Extensions\DBResource;
+
+use Psr\Cache\CacheItemPoolInterface;
+
+/**
+ * Smarty resource type to fetch template from database
+ *
+ * @package Imponeer\Smarty\Extensions\DBResource
+ */
+class DBResource extends \Smarty_Resource_Custom implements \Imponeer\Contracts\Smarty\Extension\SmartyResourceInterface
+{
+    /**
+     * @var CacheItemPoolInterface
+     */
+    private $cache;
+    /**
+     * @var \PDO
+     */
+    private $pdo;
+    /**
+     * @var string
+     */
+    private $templatesTableName;
+    /**
+     * @var string
+     */
+    private $templateSourceColumnName;
+    /**
+     * @var string
+     */
+    private $templateModificationColumnName;
+    /**
+     * @var string
+     */
+    private $tplSetName;
+    /**
+     * @var string
+     */
+    private $tplSetColumnName;
+    /**
+     * @var string
+     */
+    private $templateNameColumnName;
+    /**
+     * @var string
+     */
+    private $defaultTplSetName;
+
+    /**
+     * @var callable
+     */
+    private $templatePathGetter;
+
+    /**
+     * Constructor.
+     *
+     * @param CacheItemPoolInterface $cacheItemPool PSR-6 compatible caching instance
+     * @param \PDO $pdo PDO compatible database connection instance
+     * @param string $tplSetName Current template set name
+     * @param string $templatesTableName Table name where all template data are located
+     * @param string $templateSourceColumnName Column name that is used to store template source code
+     * @param string $templateModificationColumnName Column name that is used to store template modification Unix timestamp
+     * @param string $tplSetColumnName Column name that identifies template related teplate set for core
+     * @param string $templateNameColumnName Column name that identifies template file name
+     * @param callable $templatePathGetter Callable that is used to convert from database fetched data into real template path
+     * @param string $defaultTplSetName Default template set name
+     */
+    public function __construct(
+        CacheItemPoolInterface $cacheItemPool,
+        \PDO $pdo,
+        string $tplSetName,
+        string $templatesTableName,
+        string $templateSourceColumnName,
+        string $templateModificationColumnName,
+        string $tplSetColumnName,
+        string $templateNameColumnName,
+        callable $templatePathGetter,
+        string $defaultTplSetName = 'default'
+    ) {
+        $this->cache = $cacheItemPool;
+        $this->pdo = $pdo;
+        $this->templatesTableName = $templatesTableName;
+        $this->templateSourceColumnName = $templateSourceColumnName;
+        $this->templateModificationColumnName = $templateModificationColumnName;
+        $this->tplSetName = $tplSetName;
+        $this->tplSetColumnName = $tplSetColumnName;
+        $this->templateNameColumnName = $templateNameColumnName;
+        $this->defaultTplSetName = $defaultTplSetName;
+        $this->templatePathGetter = $templatePathGetter;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName(): string
+    {
+        return 'db';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function fetch($name, &$source, &$mtime)
+    {
+        [$source, $mtime] = $this->getInfo($name);
+    }
+
+    /**
+     * Gets info for template
+     *
+     * @param string $template Template file name
+     *
+     * @return array
+     *
+     * @throws \Psr\Cache\InvalidArgumentException
+     */
+    private function getInfo(string $template): array
+    {
+        $cacheKey = 'smarty-db-resource-' . md5($template) . '-' . strlen($template);
+        $cachedItem = $this->cache->getItem($cacheKey);
+        if ($cachedItem->isHit()) {
+            return (array)$cachedItem->get();
+        }
+
+        $data = $this->fetchTemplateDataFromDatabase($template);
+
+        if ($data === null) {
+            $cachedItem->set([null, null]);
+            $this->cache->save($cachedItem);
+            return $cachedItem->get();
+        }
+
+        if ($data[$this->tplSetColumnName] !== $this->defaultTplSetName) {
+            $cachedItem->set([
+                $data[$this->templateSourceColumnName],
+                $data[$this->templateModificationColumnName],
+            ]);
+            $this->cache->save($cachedItem);
+            return $cachedItem->get();
+        }
+
+        $file = call_user_func($this->templatePathGetter, $data);
+        $cachedItem->set([
+            file_get_contents($file),
+            filemtime($file),
+        ]);
+        $this->cache->save($cachedItem);
+
+        return $cachedItem->get();
+    }
+
+    /**
+     * Fetches template modify date info
+     *
+     * @param string $template Template to find in DB
+     *
+     * @return array|null
+     *
+     * @throws \Exception
+     */
+    private function fetchTemplateDataFromDatabase(string $template): ?array
+    {
+        $stm = $this->pdo->prepare(
+            sprintf(
+                'SELECT * FROM `%1$s` WHERE `%2$s` = :template AND `%3$s` IN (:tplset, :defaultTplSet) ORDER BY IF(`%3$s` = :defaultTplSet, 1, 0) ASC LIMIT 1',
+                $this->templatesTableName,
+                $this->templateNameColumnName,
+                $this->tplSetColumnName
+            )
+        );
+        $stm->bindValue('template', $template, \PDO::PARAM_STR);
+        $stm->bindValue('tplset', $this->tplSetName, \PDO::PARAM_STR);
+        $stm->bindValue('defaultTplSet', $this->defaultTplSetName, \PDO::PARAM_STR);
+        $stm->execute();
+
+        $row = $stm->fetch(\PDO::FETCH_ASSOC);
+        if (!$row) {
+            return null;
+        }
+
+        return $row;
+    }
+}
